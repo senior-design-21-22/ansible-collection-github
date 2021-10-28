a2b8e7b67fac5ca20962c265f5df526c557cd6c0
Nolan Khounborin <khounborinn@gmail.com>
2021-10-27 22:52:53 -0400

    Changed the original unit testing file to not be an integration test

--- tests/unit/modules/test_github_repo_return.py
+++ tests/unit/modules/test_github_repo_return.py
@@ -1,17 +1,62 @@
 import unittest
-from github import Github
+import json
+from unittest.mock import MagicMock, patch
+from ansible.module_utils import basic
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.common.text.converters import to_bytes
 
-class TestAPIConnection(unittest.TestCase):
-    def test_check_to_see_repo_returns_correctly(self):
-        g = Github('token')
-        output = []
-        org_name = 'Kegz-Tester'
+def set_module_args(args):
+    """prepare arguments so that they will be picked up during module creation"""
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
 
-        for repo in g.get_user(org_name).get_repos():
-            output.append(repo.name)
+class AnsibleExitJson(Exception):
+    """Exception class to be raised by module.exit_json and caught by the test case"""
+    pass
 
-        self.assertEqual(['repo1', 'repo2'], output)
 
-if __name__ == '__main__':
-    unittest.main()
-    
+class AnsibleFailJson(Exception):
+    """Exception class to be raised by module.fail_json and caught by the test case"""
+    pass
+
+
+def exit_json(*args, **kwargs):
+    """function to patch over exit_json; package return data into an exception"""
+    if 'changed' not in kwargs:
+        kwargs['changed'] = False
+    raise AnsibleExitJson(kwargs)
+
+
+def fail_json(*args, **kwargs):
+    """function to patch over fail_json; package return data into an exception"""
+    kwargs['failed'] = True
+    raise AnsibleFailJson(kwargs)
+
+
+def get_github_api_call(self, arg, required=False):
+    if arg.endswith('token'):
+        return 'token'
+    else:
+        if required:
+            fail_json(msg='%r not found !' % arg)
+        else:
+            return 'Invalid token'
+
+class TestMyModule(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_module_helper = patch.multiple(basic.AnsibleModule,
+                                                 exit_json=exit_json,
+                                                 fail_json=fail_json)
+        self.mock_module_helper.start()
+        self.addCleanup(self.mock_module_helper.stop)
+
+    def test_module_fail_when_required_args_missing(self):
+        set_module_args({})
+        self.assertRaises(AnsibleFailJson)
+
+    def test_pass_api_call(self):
+        assert get_github_api_call(self, arg = 'token', required = True) == 'token'
+
+        
+            
